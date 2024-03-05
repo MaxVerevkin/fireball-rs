8492015c3b08bd3588a12c1ff4ab0460f03fd97e
MaxVerevkin <maxxverrr@gmail.com>
2024-03-05 18:06:17 +0200

    fix medians

--- common/src/quick_median.rs
+++ common/src/quick_median.rs
@@ -10,30 +10,17 @@ pub trait SliceExt2<T> {
 
 impl SliceExt for [f64] {
     fn median(&mut self) -> f64 {
-        if self.is_empty() {
-            f64::NAN
-        } else if self.len() % 2 == 1 {
-            let right = *self
-                .select_nth_unstable_by(self.len() / 2, f64::total_cmp)
-                .1;
-            let left = *self
-                .select_nth_unstable_by(self.len() / 2 - 1, f64::total_cmp)
-                .1;
-            (right + left) * 0.5
-        } else {
-            *self
-                .select_nth_unstable_by(self.len() / 2, f64::total_cmp)
-                .1
-        }
+        self.median_by_key(|x| x)
     }
 }
 
 impl<T: Copy> SliceExt2<T> for [T] {
+    #[inline]
     fn median_by_key(&mut self, key_fn: impl Fn(T) -> f64) -> f64 {
         let cmp = |a: &T, b: &T| f64::total_cmp(&key_fn(*a), &key_fn(*b));
         if self.is_empty() {
             f64::NAN
-        } else if self.len() % 2 == 1 {
+        } else if self.len() % 2 == 0 {
             let right = *self.select_nth_unstable_by(self.len() / 2, cmp).1;
             let left = *self.select_nth_unstable_by(self.len() / 2 - 1, cmp).1;
             (key_fn(right) + key_fn(left)) * 0.5
